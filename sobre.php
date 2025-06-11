e47b26bf45ab3243d930bd976b997afc87a1e1b2
purpesy <lucasgabrieldossantos753@gmail.com>
2025-06-11 13:22:07 -0300

    Começo da pagina sobre

--- sobre.php
+++ sobre.php
@@ -1,6 +1,11 @@
 <?php include 'components/head.php'; ?>
-<body>
 
-<?php include 'components/header.php'; ?>
+<body>
+    <?php include 'components/header.php'; ?>
+    <?php include 'components/hero.php'; ?>
+    <main>
+    <?php include 'components/sobre.php'; ?>
+    </main>
 </body>
+
 </html>
